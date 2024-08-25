cb47dd2571739ca62fdf929d8b7dc84a4172a502
Jody Sankey <jody@jsankey.com>
2024-08-24 17:21:13 -0700

    Update flutter, deps, gradle, and SDK versions.

--- android_build.sh
+++ android_build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 flutter build appbundle
-pushd build/app/intermediates/merged_native_libs/release/out/lib
-zip -r debugsymbols.zip *
-popd
 echo Built bundle in $PWD/build/app/outputs/bundle/release/app-release.aab
-echo Built symbols in $PWD/build/app/intermediates/merged_native_libs/release/out/lib/debugsymbols.zip
+# Used in earlier versions of the gradle plugin.
+#pushd build/app/intermediates/merged_native_libs/release/out/lib
+#zip -r debugsymbols.zip *
+#popd
+#echo Built symbols in $PWD/build/app/intermediates/merged_native_libs/release/out/lib/debugsymbols.zip
